5795b1b14c67aed3ede4dd1876c496980763ab37
Charlie <charliekelly31415@gmail.com>
2019-03-18 13:00:24 -0400

    Add event handler

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,13 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+const api = require('./api')
+const ui = require('./ui')
+
+const onClickGetBooks = (event) => {
+  api.getBooks().then(ui.getBooksSuccess).catch(ui.getBooksFailure)
+}
+
 $(() => {
-  // your JS code goes here
+  $('#get-books').on('click', onClickGetBooks)
 })
