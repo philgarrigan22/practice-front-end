453a9dc0034d7973061a03aebefe1c579370a890
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-03-18 13:27:30 -0400

    Update ui to show books

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -4,12 +4,12 @@ const booksTemplate = require('./templates/button-response.handlebars')
 
 const getBooksSuccess = (responseData) => {
   const booksHtml = booksTemplate({ books: responseData.books })
-  $('#show-books').html = booksHtml
+  $('#show-books').html(booksHtml)
 }
 
 const getBooksFailure = (responseData) => {
   const message = 'Something went wrong, please try again.'
-  $('#user-message').html = message
+  $('#user-message').html(message)
 }
 
 module.exports = {
